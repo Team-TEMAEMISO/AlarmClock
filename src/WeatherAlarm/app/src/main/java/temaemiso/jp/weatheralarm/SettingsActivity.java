4f20f598b0f638d1e736d25c0021f67f633973c6
y-kawase <y-kawase@esm.co.jp>
2017-03-21 19:50:17 +0900

    ファイル追加

---
+++ src/WeatherAlarm/app/src/main/java/temaemiso/jp/weatheralarm/SettingsActivity.java
@@ -0,0 +1,95 @@
+package temaemiso.jp.weatheralarm;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
+    HashMap<String,String> data1;
+    HashMap<String,String> data2;
+    SimpleAdapter adapter;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_settings);
+
+        ListView listView = (ListView)findViewById(R.id.listView2);
+
+        createData();
+
+        adapter = new SimpleAdapter(this,list,R.layout.listview_settings,
+                new String[]{"Item",
+                        "Value"},
+                new int[]{R.id.item,
+                        R.id.value});
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){
+                    AlertDialog.Builder listDlg = new AlertDialog.Builder(SettingsActivity.this);
+                    listDlg.setTitle("繰り返し");
+                    listDlg.setItems(
+                            new String[]{"一回のみ","毎日","カスタム"},
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if(which == 0){
+                                        data1.put("Value","一回のみ");
+                                        adapter.notifyDataSetChanged();
+                                    }else if(which == 1){
+                                        data1.put("Value","毎日");
+                                        adapter.notifyDataSetChanged();
+                                    }else if(which == 2){
+                                        AlertDialog.Builder checkDlg = new AlertDialog.Builder(SettingsActivity.this);
+                                        checkDlg.setTitle("曜日選択");
+                                        checkDlg.setMultiChoiceItems(
+                                                new String[]{"日","月","火","水","木","金","土"},
+                                                new boolean[]{true,true,true,true,true,true,true},
+                                                null
+                                        );
+                                        checkDlg.setPositiveButton(
+                                                "OK",
+                                                new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+
+                                                    }
+                                                }
+                                        );
+                                        checkDlg.create().show();
+                                    }
+                                }
+                            }
+                    );
+                    listDlg.create().show();
+                }else{
+                    startActivityForResult(new Intent(SettingsActivity.this,SettingsActivity.class),1);
+                }
+            }
+        });
+    }
+
+    private void createData() {
+        data1 = new HashMap<>();
+        data1.put("Item","繰り返し");
+        data1.put("Value","月");
+        list.add(data1);
+        data2 = new HashMap<>();
+        data2.put("Item","早める時間");
+        data2.put("Value","");
+        list.add(data2);
+    }
+}
