0ed3979682bad10f0e72632d97f066c45c7a3754
tsuji <kota.tsuji@esm.co.jp>
2017-03-22 21:41:54 +0900

    アラーム時のgifアニメーション

---
+++ src/MyApplication2/app/src/main/java/com/example/kotatsuji/myapplication2/MainActivity2.java
@@ -0,0 +1,41 @@
+package com.example.kotatsuji.myapplication2;
+
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //gif表示用レイアウト
+        LinearLayout target = (LinearLayout)findViewById(R.id.ImgContainer);
+        //アプリ終了用ボタン
+        Button button = (Button)findViewById(R.id.button);
+
+        GifWebView gif = new GifWebView(this, Weather.snow);
+        //gifをインサート
+        target.addView(gif);
+
+        //クリックイベント
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity2.super.finishAndRemoveTask();
+            }
+        });
+    }
+}
---
+++ src/MyApplication2/app/src/main/java/com/example/kotatsuji/myapplication2/Weather.java
@@ -0,0 +1,8 @@
+package com.example.kotatsuji.myapplication2;
+
+public enum Weather {
+    sunny,
+    cloudiness,
+    rain,
+    snow;
+}
